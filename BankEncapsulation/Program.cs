e9bdf6fbf2b911f8013cc67502317d924ef8c8d2
Alexis Boegner <alexisboegner@gmail.com>
2025-10-24 13:23:20 -0400

    Created BankingDoWhileLoop & If-Else Statements

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Automated Teller Machine");
+            string banking = BankingDoWhileLoop();
+            
+            if (banking == "deposit")
+            {
+                // Deposit();
+            }
+            else if (banking == "withdrawal")
+            {
+                // Withdraw();
+            }
+            else if (banking == "current balance")
+            {
+                // GetBanance();
+            }
+            else //(banking == "goodbye")
+            {
+                Console.WriteLine("Goodbye");
+            }
+
+            static string BankingDoWhileLoop()
+            {
+                {
+                    string banking;
+                    string deposit = "deposit";
+                    string withdrawal = "withdrawal";
+                    string currentBalance = "current balance";
+                    string goodbye = "goodbye";
+                    
+                    do
+                    {
+                        Console.WriteLine("Please, type the word 'deposit', 'withdrawal', 'current balance', or 'goodbye'!");
+                        banking = Console.ReadLine().ToLower();
+                    }while(banking != "deposit" && banking != "withdrawal" && banking != "current balance" && banking != "goodbye");
+
+                    return banking;
+                }
+            }
         }
     }
 }
